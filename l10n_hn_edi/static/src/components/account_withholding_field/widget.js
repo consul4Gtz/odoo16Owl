1e5554440b92910916818ecaab2258dd41a295aa
consul4Gtz <consultor4@itechnologysol.com>
2024-05-28 11:14:21 -0600

    duplicado wid bueno, y corecciones wid

--- l10n_hn_edi/static/src/components/account_withholding_field/widget.js
+++ l10n_hn_edi/static/src/components/account_withholding_field/widget.js
@@ -2,21 +2,17 @@
 
 import { Component, useState } from '@odoo/owl';
 import { registry } from "@web/core/registry";
-import { xml } from '@odoo/owl/tags';
+//import { xml } from '@odoo/owl/tags';
 //import { Component } from "@odoo/owl";
 
-class MyOwlWidget extends Component {
-    static template = xml`
-        <div>
-            <span t-esc="props.value"/>
-        </div>
-    `;
-    
+export class MyOwlWidget extends Component {
+   
     setup() {
         this.state = useState({
             value: this.props.value,
         });
     }
 }
-
+MyOwlWidget .template = "l10n_hn_edi.testfield";
+MyOwlWidget .supportedTypes = ["char"];
 registry.category("fields").add('my_owl_widget', MyOwlWidget);
