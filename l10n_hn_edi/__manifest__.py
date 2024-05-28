e78f95a89192ab1d8877b91b91a906f70a4c241c
consul4Gtz <consultor4@itechnologysol.com>
2024-05-28 10:47:19 -0600

    cambios assests

--- l10n_hn_edi/__manifest__.py
+++ l10n_hn_edi/__manifest__.py
@@ -48,10 +48,11 @@
     ],
     'assets': {
         'web.assets_backend': [
-            'l10n_hn_edi/static/src/js/components/widget.js'
             'l10n_hn_edi/static/src/components/**/*',
         ],
     },
     'installable': True,
     'auto_install': False,
 }
+
+#'l10n_hn_edi/static/src/js/components/widget.js'
